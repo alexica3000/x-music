cedd3c4419df0c78f3a89f6ae60ce665b6e65384
Alex S <sanatateacom@gmail.com>
2021-09-23 21:09:44 +0300

    created admin users table

---
+++ webroot/app/Http/Livewire/Admin/UsersList.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+/**
+ * Class UsersList
+ * @package App\Http\Livewire\Admin
+ */
+class UsersList extends Component
+{
+    /**
+     * @return Application|Factory|View
+     */
+    public function render(): View|Factory|Application
+    {
+        return view('livewire.admin.users-list', ['users' => $this->getUsers()]);
+    }
+
+    /**
+     * @return LengthAwarePaginator
+     */
+    public function getUsers(): LengthAwarePaginator
+    {
+        return User::query()->paginate();
+    }
+}
--- webroot/resources/views/admin/users/index.blade.php
+++ webroot/resources/views/admin/users/index.blade.php
@@ -9,16 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                    <div class="mt-8 text-2xl">
-                        Welcome to your Jetstream application!
-                    </div>
-
-                    <div class="mt-6 text-gray-500">
-                        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                        ecosystem to be a breath of fresh air. We hope you love it.
-                    </div>
+                    <livewire:admin.users-list />
                 </div>
             </div>
         </div>
