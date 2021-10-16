f22f6aaf32becd86b49a0d639a9bc56a2fc58b66
Alex S <sanatateacom@gmail.com>
2021-10-16 16:48:55 +0300

    added id for tags type list

--- webroot/resources/views/livewire/admin/tags-type-list.blade.php
+++ webroot/resources/views/livewire/admin/tags-type-list.blade.php
@@ -9,6 +9,7 @@
                 <table class="min-w-max w-full table-auto">
                     <thead>
                     <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                        <th class="py-3 px-6 text-left">ID</th>
                         <th class="py-3 px-6 text-left">Name</th>
                         <th class="py-3 px-6 text-center">Sort</th>
                         <th class="py-3 px-6 text-center">Score</th>
@@ -18,6 +19,9 @@
                     <tbody class="text-gray-600 text-sm font-light">
                     @foreach($tagTypes as $type)
                         <tr class="border-b border-gray-200 hover:bg-gray-100">
+                            <td class="py-3 px-6 text-left">
+                                <span>{{ $type->id }}</span>
+                            </td>
                             <td class="py-3 px-6 text-left">
                                 <span>{{ $type->name }}</span>
                             </td>
