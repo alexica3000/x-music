5a94250ce4f4b844aaf8e9dc5bd5ccf05b0b0fcb
Alex S <sanatateacom@gmail.com>
2021-11-01 23:49:40 +0200

    search for tracks

--- webroot/app/Http/Livewire/Admin/Tracks/TracksList.php
+++ webroot/app/Http/Livewire/Admin/Tracks/TracksList.php
@@ -3,6 +3,8 @@
 namespace App\Http\Livewire\Admin\Tracks;
 
 use App\Models\Track;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\View\View;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -15,14 +17,43 @@ class TracksList extends Component
 {
     use WithPagination;
 
-    public function render(): View
+    public string $search;
+    public string $active;
+
+    public function mount()
     {
-        return view('livewire.admin.tracks.tracks-list', ['tracks' => $this->getTracks()]);
+        $this->resetValues();
     }
 
-    private function getTracks()
+    /**
+     * @return View
+     */
+    public function render(): View
+    {
+        return view('livewire.admin.tracks.tracks-list', [
+            'tracks' => $this->getTracks(),
+        ]);
+    }
+
+    /**
+     * @return LengthAwarePaginator
+     */
+    public function getTracks(): LengthAwarePaginator
     {
         return Track::query()
+            ->when($this->search, fn(Builder $query) => $query->where('name', 'like', "%$this->search%"))
+            ->when($this->active != null, fn(Builder $query) => $query->where('is_live', $this->active))
             ->paginate();
+    }
+
+    public function updatingSearch(): void
+    {
+        $this->resetPage();
+    }
+
+    public function resetValues(): void
+    {
+        $this->search = '';
+        $this->active = '';
     }
 }
--- webroot/resources/views/livewire/admin/tracks/tracks-list.blade.php
+++ webroot/resources/views/livewire/admin/tracks/tracks-list.blade.php
@@ -4,19 +4,19 @@
 
 <div class="overflow-x-auto">
     <div class="flex m-3">
-{{--        <div class="relative text-gray-600">--}}
-{{--            <input--}}
-{{--                type="search"--}}
-{{--                placeholder="Search"--}}
-{{--                class="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"--}}
-{{--                wire:model="search"--}}
-{{--            >--}}
-{{--            <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">--}}
-{{--                <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">--}}
-{{--                  <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>--}}
-{{--                </svg>--}}
-{{--            </button>--}}
-{{--        </div>--}}
+        <div class="relative text-gray-600">
+            <input
+                type="search"
+                placeholder="Search"
+                class="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
+                wire:model="search"
+            >
+            <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
+                <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
+                  <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
+                </svg>
+            </button>
+        </div>
 {{--        <div class="ml-5 w-1/6">--}}
 {{--            <label>--}}
 {{--                <select class="form-select block w-full rounded shadow-md border-gray-400 h-10" wire:model="tagTypeId">--}}
@@ -27,24 +27,24 @@
 {{--                </select>--}}
 {{--            </label>--}}
 {{--        </div>--}}
-{{--        <div class="ml-5 w-1/6">--}}
-{{--            <label>--}}
-{{--                <select class="form-select block w-full rounded shadow-md border-gray-400 h-10" wire:model="active">--}}
-{{--                    <option value="">Is live</option>--}}
-{{--                    <option value="1">Active</option>--}}
-{{--                    <option value="0">Inactive</option>--}}
-{{--                </select>--}}
-{{--            </label>--}}
-{{--        </div>--}}
-{{--        <div class="ml-5 w-1/6">--}}
-{{--            <label>--}}
-{{--                <button--}}
-{{--                    type="button"--}}
-{{--                    class="block w-full rounded shadow-md border-green-400 h-10 bg-green-400 hover:bg-green-300 text-white"--}}
-{{--                    wire:click="resetValues()"--}}
-{{--                >Reset</button>--}}
-{{--            </label>--}}
-{{--        </div>--}}
+        <div class="ml-5 w-1/6">
+            <label>
+                <select class="form-select block w-full rounded shadow-md border-gray-400 h-10" wire:model="active">
+                    <option value="">Is live</option>
+                    <option value="1">Active</option>
+                    <option value="0">Inactive</option>
+                </select>
+            </label>
+        </div>
+        <div class="ml-5 w-1/6">
+            <label>
+                <button
+                    type="button"
+                    class="block w-full rounded shadow-md border-green-400 h-10 bg-green-400 hover:bg-green-300 text-white"
+                    wire:click="resetValues()"
+                >Reset</button>
+            </label>
+        </div>
     </div>
 
     <div class="min-w-screen bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
@@ -72,9 +72,9 @@
                             <td class="py-2 px-6 text-left">
                                 master...
                             </td>
-                            <td class="py-2 px-6 text-left">
+                            <td class="py-2 px-6 text-center">
                                 @if($track->is_live)
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                                     </svg>
                                 @endif
